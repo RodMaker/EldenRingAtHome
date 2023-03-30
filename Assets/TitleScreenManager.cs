93bec459ce6af64e0896df74a73f197072e66424
RodMaker <83619630+RodMaker@users.noreply.github.com>
2023-03-30 03:09:47 +0100

    Episode1

---
+++ Assets/TitleScreenManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+namespace RM
+{
+    public class TitleScreenManager : MonoBehaviour
+    {
+        // Starts the network as a host
+        public void StartNetworkAsHost()
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+
+        public void StartNewGame()
+        {
+            StartCoroutine(WorldSaveGameManager.instance.LoadNewGame());
+        }
+    }
+
+}
